1b8096d9b42f09ccd8fa386dfd3774831783cedf
Marina Friauf <marina506506506@gmail.com>
2023-03-18 14:11:04 +0100

    test y data

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,54 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{"Майкоп", "Уфа", "Новосибирск", "Москва", "Томск", "Якутск"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale("ru"));
+        String name = faker.name().lastName() + " " + faker.name().firstName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale("ru"));
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            UserInfo user = new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+
+    }
+}
